830d4efa3d76195a2f307a085ff33025d82ffc17
c0dex1337 <bycrypton_1239@hotmail.com>
2020-10-31 02:19:31 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,9 +12,10 @@
 Config = {}
 
 Config.ModTusu         = 210        -- Modlar arasında geçiş yapmak için kullanılan tuş. (https://docs.fivem.net/docs/game-references/controls/)
-Config.HizMiktari      = 25.0       -- Araç spor moddayken motor hızının kaç kat artacağı. Sonuna .0 koymak zorunlu. Varsayılan değer 25.0
+Config.HizMiktari      = 35.0       -- Araç spor moddayken motor hızının kaç kat artacağı. Sonuna .0 koymak zorunlu. Varsayılan değer 25.0
 Config.MaxHizMiktari   = 999.0      -- Araç spor modundayken aracın maximum kaç kmde gidebileceği. Sonuna .0 koymak zorunlu. Varsayılan değer 999.0
-Config.BaglamaSuresi   = 35 * 1000  -- Spor modunun araca kaç saniyede bağlanacağı. 35'i 60 yaparsanız 60 saniyede bağlar. Varsayılan değer 35.
-Config.CikarmaSuresi   = 35 * 1000  -- Spor modunun araçtan kaç saniyede çıkartılacağı. 35'i 60 yaparsanız 60 saniyede bağlar. Varsayılan değer 35.
-Config.Cikartilabilir  = true       -- Eğer true yaparsanız spor mode sportmoderemover itemiyle çıkartılabilir. Eğer false yaparsanız çıkartılamaz. Varsayılan değer true.
+Config.DokmeSuresi     = 35 * 1000  -- Zx3 maddesinin aracın benzin deposuna kaç saniyede döküleceği. 35'i 60 yaparsanız 60 saniyede döker. Varsayılan değer 35.
+Config.CikarmaSuresi   = 35 * 1000  -- Zx3 maddesinin aracın benzin deposundan kaç saniyede çıkartılacağı. 35'i 60 yaparsanız 60 saniyede çıkartır. Varsayılan değer 35.
+Config.Cikartilabilir  = true       -- Eğer true yaparsanız spor mode zx3remover itemiyle çıkartılabilir. Eğer false yaparsanız çıkartılamaz. Varsayılan değer true.
 Config.BeklemeSuresi   = 5 * 1000   -- Modlar arasında geçiş yaparken bekleme süresi. 5 yerine 15 yazarsanız 15 saniyede bir modlar arası geçiş yapılabilir. Devre dışı bırakmak için 5 yerine 0 yazın. Varsayılan değer 5.
+Config.KontrolSuresi   = 35 * 1000  -- Zx3 maddesinin aracın benzin deposunda olup olmadığını kontrol eden sistemin kaç saniyede kontrol edeceği. 35'i 60 yaparsanız 60 saniyede kontrol eder. Varsayılan değer 35.
