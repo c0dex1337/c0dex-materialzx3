4f52f9ef5d1083ce1980754af0f3dad7728d8fe0
c0dex1337 <bycrypton_1239@hotmail.com>
2020-10-31 02:20:14 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,8 +1,9 @@
 INSERT INTO `items` (`name`, `label`, `weight`) VALUES
-	('sportmode', 'Spor Modu', 1),
-	('sportmoderemover', 'Spor Mod Çıkartıcı', 1)
+	('zx3', 'ZX3 Maddesi', 1),
+	('zx3checker', 'ZX3 Kontrol Aleti', 1),
+	('zx3remover', 'ZX3 Çıkartma Aleti', 1)
 ;
 
-CREATE TABLE IF NOT EXISTS `spor_mode` (
+CREATE TABLE IF NOT EXISTS `material_zx3` (
   `plate` tinytext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
